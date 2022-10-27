cab57564f504f8891c17ef68936d404f3b967195
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-27 11:35:35 +0900

    QueryParam

---
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -0,0 +1,32 @@
+package com.springboot.hello.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String getHello(){
+        return "hello world";
+    }
+    @GetMapping(value="/name")
+    public String getName(){
+        return "Minji";
+    }
+    @GetMapping(value="/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+    @GetMapping(value="/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+    @GetMapping(value="/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+        //return name+" "+email+" "+organization;
+        return String.format("%s %s %s", name, email, organization);
+    }
+}
