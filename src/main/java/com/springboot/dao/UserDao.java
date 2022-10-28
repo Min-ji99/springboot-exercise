6222c376338031b54b0a2cde6c2f88246003060b
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-28 13:57:37 +0900

    UserDao add, deleteAll, deleteById, findById 추가

---
+++ src/main/java/com/springboot/dao/UserDao.java
@@ -0,0 +1,43 @@
+package com.springboot.dao;
+
+import com.springboot.domain.User;
+import com.springboot.domain.dto.UserRequestDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class UserDao {
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(final UserRequestDto userRequestDto){
+        this.jdbcTemplate.update("insert into users(id, email, password) values (?, ?, ?)",
+                userRequestDto.getId(), userRequestDto.getName(), userRequestDto.getPassword());
+    }
+    public void deleteAll(){
+        this.jdbcTemplate.update("delete from users");
+    }
+    public void deleteById(String id){
+        this.jdbcTemplate.update("delete from users where id=?", id);
+    }
+    public User findById(String id){
+        RowMapper<User> rowMapper=new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user=new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                return user;
+            }
+        };
+        return this.jdbcTemplate.queryForObject("select * from users where id=?", rowMapper, id);
+    }
+}
