681d99ecd15dcf55ab4281fc4c461ba465038fca
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-28 13:46:07 +0900

    User 추가

---
+++ src/main/java/com/springboot/domain/dto/User.java
@@ -0,0 +1,24 @@
+package com.springboot.domain.dto;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    public User(String name, String email, String password){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
