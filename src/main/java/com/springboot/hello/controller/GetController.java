3d232f32f5d98e438aef85012f47bdf8511894ee
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-28 11:41:27 +0900

    logback 적용 log.info 사용해보기

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,15 +1,18 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
     @RequestMapping(value="/hello", method= RequestMethod.GET)
     public String getHello(){
+        log.info("hello 요청이 들어왔습니다");
         return "hello world";
     }
     @GetMapping(value="/name")
@@ -18,10 +21,12 @@ public class GetController {
     }
     @GetMapping(value="/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
+        log.info("getVariable1 요청이 들어왔습니다.");
         return variable;
     }
     @GetMapping(value="/variable2/{variable}")
     public String getVariable2(@PathVariable("variable") String var){
+        log.info("getVariable1 요청이 들어왔습니다. variable : {}", var);
         return var;
     }
     @GetMapping(value="/request1")
