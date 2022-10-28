58e05883e07d61e56d1e043744326d7731918240
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-28 13:48:20 +0900

    User getId 수정

--- src/main/java/com/springboot/domain/dto/User.java
+++ src/main/java/com/springboot/domain/dto/User.java
@@ -2,16 +2,16 @@ package com.springboot.domain.dto;
 
 public class User {
     private String name;
-    private String email;
+    private String id;
     private String password;
-    public User(String name, String email, String password){
+    public User(String id, String name, String password){
         this.name=name;
-        this.email=email;
+        this.id=id;
         this.password=password;
     }
 
-    public String getEmail() {
-        return email;
+    public String getId() {
+        return id;
     }
 
     public String getName() {
